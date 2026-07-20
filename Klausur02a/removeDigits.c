c3b74f25e0d38acffd60b827c63f351f01b9968b
Hamza Mirza <hamza665@hotmail.de>
2026-07-20 15:50:44 +0200

    solution for removeDigits

--- Klausur02a/removeDigits.c
+++ Klausur02a/removeDigits.c
@@ -5,26 +5,61 @@
 */
 #include "base.h"
 
-bool isDigit(char c){
+bool isDigit(char c)
+{
 	return c <= '9' && c >= '0';
 }
 
-char* removeDigits(char* s){
-	return "Error";
+char *removeDigits(char *s)
+{
+	if (s[0] == '\n')
+	{
+		return s;
+	}
+
+	int count = 0;
+	for (int i = 0; s[i] != '\0'; i++)
+	{
+		char c = s[i];
+
+		if (!isDigit(c))
+		{
+			count++;
+		}
+	}
+
+	char *result = xmalloc(count + 1);
+
+	int j = 0;
+	for (int i = 0; s[i] != '\0'; i++)
+	{
+		char c = s[i];
+
+		if (!isDigit(c))
+		{
+			result[j] = c;
+			j++;
+		}
+	}
+
+	result[j] = '\0';
+
+	return result;
 }
 
-void test_removeDigits(void) {
-    test_equal_s(removeDigits(""), "");
+void test_removeDigits(void)
+{
+	test_equal_s(removeDigits(""), "");
 	test_equal_s(removeDigits("x"), "x");
 	test_equal_s(removeDigits("11"), "");
 	test_equal_s(removeDigits("1x1x"), "xx");
 	test_equal_s(removeDigits("111x111"), "x");
 	test_equal_s(removeDigits("1a2b3c4"), "abc");
 	test_equal_s(removeDigits("101191xk"), "xk");
- }
- 
-int main(void) {
-    test_removeDigits();
-	report_memory_leaks(true);
-    return 0;
+}
+
+int main(void)
+{
+	test_removeDigits();
+	return 0;
 }
